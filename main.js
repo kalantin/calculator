b79660efb8aab37f2692f44cf4ef68d14edf839a
kalantin <kalantin@gmail.com>
2022-02-20 19:28:11 +0100

    change layout

---
+++ main.js
@@ -0,0 +1,13 @@
+const btnPlus = document.querySelector('.btnPlus');
+const btnMinus = document.querySelector('.btnMinus');
+const btnMulti = document.querySelector('.btnMulti');
+const btnSplit = document.querySelector('.btnSplit');
+const btnClear = document.querySelector('.btnClear');
+const btnResult = document.querySelector('.btnResult');
+const inputA = document.querySelection('.inputA')
+const inputB = document.querySelection('.inputB')
+const result = document.querySelector('.result')
+
+const plus = () =>{
+
+}
